fccc300d452af9c15a176b714617ba29589eb1e5
Kagisho Makoti <kagishomakoti@outlook.com>
2026-01-04 08:48:18 +0200

    Fix: Fix app crash due to port configuration
    App hosted on render, App preset port is different to what render sets it to. This caused server timeout only on hosted server.

--- main.py
+++ main.py
@@ -305,4 +305,5 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    port = os.environ.get(key='PORT',default=5000)
+    app.run(host='0.0.0.0', port=port, debug=False)
